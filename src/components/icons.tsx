8fbbd3d81bb83a99fa47020c6e70d2bbddc0b7fa
mikedevv <maungdevv@gmail.com>
2025-10-26 10:07:47 +0630

    fix: vite build

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -225,18 +225,19 @@ export const StickerIcon = (props: IconSvgProps) => (
   </svg>
 );
 
-export const SendIcon = (props: IconSvgProps) => (
+export const SendIcon = ({ size = 24, ...props }: IconSvgProps) => (
   <svg
     className="lucide lucide-send-horizontal-icon lucide-send-horizontal"
     fill="none"
-    height="24"
+    height={size}
+    width={size}
     stroke="currentColor"
     strokeLinecap="round"
     strokeLinejoin="round"
     strokeWidth="2"
     viewBox="0 0 24 24"
-    width="24"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z" />
     <path d="M6 12h16" />
